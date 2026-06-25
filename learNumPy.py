4d74253bcd1a70b0ed29614c9e912996e9da47c4
Nijo <ngsam.nijo@gmail.com>
2026-06-25 13:44:30 +0530

    note

--- learNumPy.py
+++ learNumPy.py
@@ -1,6 +1,5 @@
 import numpy as np
-nparr=np.array([[[1,2,34,23],[2,34,81,11],[21,83,67,34]],[[4,13,46,176],[12,989,45,213],[124,55,51,135]]])
-print(nparr)
-print(nparr.shape)
-print(nparr.itemsize)
-print(nparr.arr.T)
+
+
+array = np.array([[3,6,2],[34,63,4]])
+print (array.shape)
